0097ff200183d6d3b6f37213f68de14ad531e912
s18774 <s18774@pjwstk.edu.pl>
2023-12-06 19:26:37 +0100

    random stuuf

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const { check, validationResult } = require('express-validator');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
@@ -19,24 +22,45 @@ app.get('/', (req, res) => {
 app.post('/submit', [
   check('imie').isLength({ min: 2, max:20 }).withMessage('Imię musi zawierać co najmniej 2 znaki i nie więcej niż 20'),
   check('wiek').custom(value => {
-    if (isNaN(value) || value < 18 || value >100) {
+    if (isNaN(value) || value < 18 || value > 100) {
       throw new Error('Musisz mieć co najmniej 18 lat, ale też nie więcej niż 100 xd');
     }
     return true;
   }),
   check('email').isEmail().withMessage('Niepoprawny format adresu email.'),
-], (req, res) => {
+], (req, res, next) => {
   const { imie, wiek, email } = req.body;
 
   const errors = validationResult(req);
 
   if (errors.isEmpty()) {
-     return res.render('results', { imie, wiek, email }); 
+    return res.render('results', { imie, wiek, email });
   }
 
-  return res.render('form', {
-    errors: errors.array()
-  });
+  // Jeśli są błędy, przekazujemy do obsługi błędów
+  next(errors.array());
+});
+
+// Dodajemy middleware do obsługi błędów
+app.use((err, req, res, next) => {
+  res.render('form', { errors: err });
+});
+
+// Dodajemy middleware do zapisu błędów do pliku
+app.use((err, req, res, next) => {
+    console.log("widzisz mnie?");
+  if (err instanceof Error) {
+    const errorLog = `Date and Time: ${new Date().toLocaleString()}\nMethod: ${req.method}\nURL: ${req.originalUrl}\nError: ${err.message}\n\n`;
+
+    // Zapis do pliku
+    fs.writeFile('test.txt', errorLog, (fileErr) => {
+      if (fileErr) {
+        console.error('Błąd podczas zapisu do pliku error.log:', fileErr);
+      }
+    });
+  }
+
+  next(err);
 });
 
 app.listen(port, () => {
